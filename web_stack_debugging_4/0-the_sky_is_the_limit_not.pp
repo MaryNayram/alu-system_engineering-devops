cabf796470431438755a90b969547cfd9027693a
MaryNayram <m.doh@alustudent.com>
2023-08-25 13:12:58 +0000

    0-the_sky_is_the_limit_not.pp

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,3 +4,4 @@ exec { 'ULIMIT':
     command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
       path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
       }
+      
