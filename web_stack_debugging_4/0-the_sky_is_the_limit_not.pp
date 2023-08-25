8cde49967a42661a20b16c858b712e1a0f2a61a0
MaryNayram <m.doh@alustudent.com>
2023-08-25 09:25:14 +0000

    0-the_sky_is_the_limit_not.pp

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,6 @@
-# This manuscript increases the amount of traffic an Nginx server can handle
-
-# Increase the ULIMIT of the default file
-file { 'fix-for-nginx':
-  ensure  => 'file',
-    path    => '/etc/default/nginx',
-      content => inline_template('<%= File.read("/etc/default/nginx").gsub(/15/, "4096") %>'),
+#Sky is the limit, let's bring that limit higher
+exec { 'ULIMIT':
+  onlyif  => 'test -e /etc/default/nginx',
+    command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+      path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
       }
-
-# Restart Nginx
--> exec { 'nginx-restart':
-  command => 'nginx restart',
-    path    => '/etc/init.d/',
-    }
